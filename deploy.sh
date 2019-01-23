e06179d22eb0154f0199aaaefd5683b2157ab20c
Diego Roso da Silva <silva95diego@hotmail.com>
2019-01-23 21:36:49 -0200

    travis: config for execute deploy if branch master

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,12 @@ npm run build
 # pull requests e commits para outras branches diferentes da master
 # não devem fazer o deploy, isso é opcional caso queira deletar as próximas 6 linhas
 # fique a vontade
-# SOURCE_BRANCH="master"
+SOURCE_BRANCH="master"
 
-# if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-#     echo "Skipping deploy."
-#     exit 0
-# fi
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy."
+    exit 0
+fi
 
 # entre na pasta onde está o build do seu projeto e inicie um novo repositório git
 cd build
